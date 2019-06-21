6d4ba3499dd6e1dea34430e44b27e6a011a3fbdc
liliang <649743135@qq.com>
2019-06-21 13:54:58 +0800

    write update sql

--- trans.py
+++ trans.py
@@ -4,8 +4,6 @@ from googletrans import Translator
 
 import MySQLdb
 
-import configparser
-
 conn = MySQLdb.connect(host='127.0.0.1', port=3306, user='root', passwd='root', db='soc', charset='utf8')
 
 cursor = conn.cursor()
@@ -14,18 +12,14 @@ cursor.execute("select id,name from asset_category")
 
 rs = cursor.fetchall()
 
-i18n = configparser.ConfigParser()
-
-i18n.add_section('AssetCategory')
-
+sql_list = []
 if len(rs) > 0:
     translator = Translator(proxies={"http": "http://127.0.0.1:23599",
                                      "https": "http://127.0.0.1:23599"})
     for row in rs:
         value = string.capwords(translator.translate(row[1]).text)
-        i18n.set('AssetCategory', "AssetCategory."+str(row[0]), value)
+        sql_list.append("update asset_category set name = '"+value+"' where id = "+str(row[0])+"; \n")
 
-# Writing our configuration file to 'example.cfg'
-with open('example.properties', 'w') as configfile:
-    i18n.write(configfile)
+with open('update.sql', 'w') as configfile:
+    configfile.writelines(sql_list)
 
